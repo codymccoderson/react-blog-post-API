0f1d66dd6ef0a145405511d36f8e6fc37f74b6c0
Peter Jones <peteflux@gmail.com>
2020-04-02 11:25:37 -0400

    initial commit with correct file structure

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express'); 
+    path = require('path');
+    cookieParser = require('cookie-parser');
+    logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+    usersRouter = require('./routes/users');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
