78f51225f12155d20fcbdbb858f0601e397fc401
Peter Jones <peteflux@gmail.com>
2020-04-02 15:37:29 -0400

    async functions for json data written and minor additions to seed file to populate db

--- routes/version1.js
+++ routes/version1.js
@@ -1,5 +1,28 @@
 const express = require("express");
     router = express.Router();
 
+const PostModel = require("../models/posts");
+
+router.get("/", (request, response, next) => {
+    response.send("Welcome to my API").status(200);
+});
+
+// Create a post
+
+// Read All
+
+router.get("/all", async (request, response) => {
+    const getAllPosts = await PostModel.getAll();
+    response.json(getAllPosts).status(200);
+});
+
+// Read One
+
+router.get("/post/:p_id?", async (request, response) => {
+    const postId = request.params.p_id;
+    const theActualPost = await PostModel.getPostById(postId);
+    response.json(theActualPost).status(200);
+});
+
 
 module.exports = router;
--- seed.sql
+++ seed.sql
@@ -1,2 +1,4 @@
-INSERT INTO authors(id, name, email) VALUES (1, 'Pete', 'pete@digitalcrafts.com');
+INSERT INTO authors (id, name, email) VALUES (1, 'Pete', 'pete@digitalcrafts.com');
 INSERT INTO posts (id, title, author_id, content) VALUES (1, 'My first blog post', 1 , 'This is my first blog post');
+INSERT INTO authors (id, name, email) VALUES (2, 'Karley', 'karley@digitalcrafts.com'), (3, 'Geordy', 'geordy@dustinfootfetish.com');
+INSERT INTO posts (id, title, author_id, content) VALUES (2, 'New Games Dropped', 2, 'Been playing The Sims and DOOM for kicks'), (3, 'There are no sports', 3, 'I used to watch NBA basketball and now that is no longer possible');
